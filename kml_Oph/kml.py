9818d3a79755cb98c9f024153c7bd3ff8004b0cb
rudolffu <fuympku@outlook.com>
2023-02-13 23:58:21 +0800

    add .gitignore; minor code revision

--- kml_Oph/kml.py
+++ kml_Oph/kml.py
@@ -4,15 +4,22 @@ import pytz
 import simplekml
 import pandas as pd
 import sys
+from pathlib import Path
+
 file = sys.argv[1]
-time = datetime.datetime.now(tz=pytz.timezone('Asia/Shanghai')).strftime("%Y-%m-%d-h%H")
+time = datetime.datetime.now(tz=pytz.timezone('Asia/Shanghai'))
+time_str = time.replace(tzinfo=None).isoformat(timespec='hours')
 data = pd.read_excel(file, index_col=0)
+file_stem = Path(file).stem
 kml = simplekml.Kml()
-for i in data.index:
+
+for i, coord_str in enumerate(data.Coordinate):
     pnt = kml.newpoint()
     pnt.name = i
-    coords = tuple([float(i) for i in data['Coordinate'][i].strip('(').strip(')').split(',')])
+    coord_str = coord_str.strip('()').split(',')
+    coords = tuple([float(x) for x in coord_str])
     pnt.coords = [coords]
-    pnt.description = description=data['Description'][i]
-    pnt.style.iconstyle.icon.href = 'images/icon-' + data['Type'][i] + str(data['Priority'][i]) +'.png'
-kml.savekmz(file[:-5] + '--' + time +'.kmz')
+    pnt.description = data['Description'][i]
+    pnt.style.iconstyle.icon.href = 'images/icon-{}{}.png'.format(data['Type'][i], data['Priority'][i])
+output_name = '{}-{}.kmz'.format(file_stem, time_str)
+kml.savekmz(output_name)
